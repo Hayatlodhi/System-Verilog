9b8575375f48af5ed9ef0942e5e9a6957fcd0150
Muhammad Hayat <96942909+Hayatlodhi@users.noreply.github.com>
2023-02-28 20:15:58 +0500

    Update Day_3_Event.sv

--- Day_3_Event.sv
+++ Day_3_Event.sv
@@ -1,1 +1,20 @@
 
+// Code your testbench here
+// or browse Examples
+module tb;
+  event hayat_event;
+  initial begin 
+    #10 ->hayat_event;
+    $display("trigerred event at time %0t",$time);
+  end
+    initial begin 
+      $display(" waiting for trigerred event at time %0t",$time);
+      @(hayat_event);
+      $display("trigerred event at time after recieving  %0t",$time);
+  end
+endmodule
+
+//Output
+// waiting for trigerred event at time 0
+// trigerred event at time 10
+// trigerred event at time after recieving  10
