e48df395973041832abff1dd5b8d7a4236117089
Muhammad Hayat <96942909+Hayatlodhi@users.noreply.github.com>
2023-03-01 19:42:36 +0500

    Create function.sv

---
+++ function.sv
@@ -0,0 +1,13 @@
+module hayat_function;
+  initial begin
+  int s;
+   s= sum(3,4);
+    $display ("sum=%0d",s);
+    #10
+    s=sum(3,s);
+    $display ("2nd time sum=%0d",s);
+  end
+  function byte sum(input int x,y);
+    sum=x+y; 
+  endfunction
+endmodule
